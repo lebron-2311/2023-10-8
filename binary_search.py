64971ea6cf1e2b4f0db178a6cf07c235b10ab634
lebron-2311 <139559344+lebron-2311@users.noreply.github.com>
2023-10-09 10:54:20 +0800

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+
+mylist = [1, 3, 5, 7, 9]
+print(binary_search(mylist, 3))  # => 1
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
